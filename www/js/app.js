802a2bbe9a5aa92ea57812c622e925595ba3346c
Matthias Kurz <mk@mediamachine.de>
2016-05-20 15:42:11 +0200

    initial

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,18 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
-
-	function renderHomeView() {
-		$('body').html(homeTpl());
-		$('.search-key').on('keyup', findByName);
-	}
-
-
-
 	/* ---------------------------------- Local Variables ---------------------------------- */
-	var homeTpl = Handlebars.compile($("#home-tpl").html());
-	var sessionListTpl = Handlebars.compile($("#session-list-tpl").html());
+	HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+	SessionListView.prototype.template = Handlebars.compile($("#session-list-tpl").html());
 
 	var service = new ConferenceService();
 	service.initialize().done(function () {
-		renderHomeView();
+		$('body').html(new HomeView(service).render().$el);
 	});
 
 	/* --------------------------------- Event Registration -------------------------------- */
-
 	document.addEventListener('deviceready', function () {
+		FastClick.attach(document.body);
 		if (navigator.notification) { // Override default HTML alert with native dialog
 			window.alert = function (message) {
 				navigator.notification.alert(
@@ -31,16 +23,8 @@
 				);
 			};
 		}
-		;
-		FastClick.attach(document.body);
 	}, false);
 
-
 	/* ---------------------------------- Local Functions ---------------------------------- */
-	function findByName() {
-		service.findByName($('.search-key').val()).done(function (sessions) {
-			$('.content').html(sessionListTpl(sessions));
-		});
-	}
 
 }());
