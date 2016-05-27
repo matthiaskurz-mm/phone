5a22f1550a826f0a7b5c3c156de977620c89eae2
Matthias Kurz <mk@mediamachine.de>
2016-05-27 14:26:22 +0200

    initial

---
+++ www/js/geolocation.js
@@ -0,0 +1,24 @@
+$(function () {
+	document.addEventListener("deviceready", onDeviceReady, false);
+})
+
+function onDeviceReady() {
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+
+// onSuccess Geolocation
+
+function onSuccess(position) {
+	var element = document.getElementById('geolocation');
+	element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+		'Longitude: ' + position.coords.longitude + '<br />'
+
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+	alert('code: ' + error.code + '\n' +
+		'message: ' + error.message + '\n');
+}
