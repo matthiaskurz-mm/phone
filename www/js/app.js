c1756616d345ba616459ab814b94f680a7b87969
Matthias Kurz <mk@mediamachine.de>
2016-06-17 14:17:20 +0200

    initial

--- www/js/app.js
+++ www/js/app.js
@@ -47,20 +47,10 @@
 	/* --------------------------------- Event Registration -------------------------------- */
 	document.addEventListener("deviceready", onDeviceReady, false);
 	document.addEventListener('deviceready', function () {
-		alert("ready");
 
 		navigator.geolocation.getCurrentPosition(onSuccess, onError);
 		FastClick.attach(document.body);
-		if (navigator.notification) { // Override default HTML alert with native dialog
-			window.alert = function (message) {
-				navigator.notification.alert(
-					message,    // message
-					null,       // callback
-					"Workshop", // title
-					'OK'        // buttonName
-				);
-			};
-		}
+
 
 	}, false);
 
@@ -113,7 +103,17 @@
 	};
 
 	function onDeviceReady() {
-		alert("navigator.geolocation works well");
+		alert("navigator!");
+		if (navigator.notification) { // Override default HTML alert with native dialog
+			window.alert = function (message) {
+				navigator.notification.alert(
+					message,    // message
+					null,       // callback
+					"Workshop", // title
+					'OK'        // buttonName
+				);
+			};
+		}
 	}
 
 }());
