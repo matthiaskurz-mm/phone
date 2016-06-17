db5bee0d3dfb20e542f291f59547e01b03bccfa0
Matthias Kurz <mk@mediamachine.de>
2016-06-17 15:10:19 +0200

    initial

--- www/js/app.js
+++ www/js/app.js
@@ -63,14 +63,14 @@
 	//
 	function onSuccess(position) {
 		var element = document.getElementById('geolocation');
-		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+		alert('Latitude: ' + position.coords.latitude + '<br />' +
 			'Longitude: ' + position.coords.longitude + '<br />' +
 			'Altitude: ' + position.coords.altitude + '<br />' +
 			'Accuracy: ' + position.coords.accuracy + '<br />' +
 			'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
 			'Heading: ' + position.coords.heading + '<br />' +
 			'Speed: ' + position.coords.speed + '<br />' +
-			'Timestamp: ' + position.timestamp + '<br />';
+			'Timestamp: ' + position.timestamp + '<br />');
 	}
 
 	// onError Callback receives a [PositionError](PositionError/positionError.html) object
