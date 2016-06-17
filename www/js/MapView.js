9875c095c84cbe5df8a7578ad0c9dd248bde9a00
Matthias Kurz <mk@mediamachine.de>
2016-06-17 15:39:52 +0200

    initial

--- www/js/MapView.js
+++ www/js/MapView.js
@@ -11,8 +11,26 @@ var MapView = function (session) {
 	};
 
 	this.initialize();
-
 	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	// onSuccess Geolocation
+	//
+	function onSuccess(position) {
+		var element = document.getElementById('geolocation');
+		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+			'Longitude: ' + position.coords.longitude + '<br />' +
+			'Altitude: ' + position.coords.altitude + '<br />' +
+			'Accuracy: ' + position.coords.accuracy + '<br />' +
+			'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
+			'Heading: ' + position.coords.heading + '<br />' +
+			'Speed: ' + position.coords.speed + '<br />' +
+			'Timestamp: ' + position.timestamp + '<br />';
+	}
 
+	// onError Callback receives a [PositionError](PositionError/positionError.html) object
+	//
+	function onError(error) {
+		alert('code: ' + error.code + '\n' +
+			'message: ' + error.message + '\n');
+	}
 
 }
